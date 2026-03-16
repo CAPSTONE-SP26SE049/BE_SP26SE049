6997b46ac0b76a590289d8fdb55955adc4212b73
Nguyen Duc Tuan <tuanndse173318@fpt.edu.vn>
2026-03-16 08:30:09 +0700

    feat: Implement generic data import/export functionality, refactor base CRUD components, and update entities and services with schema changes.

---
+++ src/main/java/org/fsa_2026/company_fsa_captone_2026/base/crud/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.example.starter_project_2025.base.crud;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/org/fsa_2026/company_fsa_captone_2026/dto/ExcelColumn.java
@@ -0,0 +1,4 @@
+package org.fsa_2026.company_fsa_captone_2026.dto;
+
+public @interface ExcelColumn {
+}
